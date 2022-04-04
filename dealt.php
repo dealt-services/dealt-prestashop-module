a664dc4c13d79b7b7bbada9e4075ed3b94b2a293
Edvin CANDON <edvincandon@gmail.com>
2022-04-04 15:55:55 +0200

    chore: base prestashop module setup

---
+++ dealt.php
@@ -0,0 +1,41 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+  exit;
+}
+
+class Dealt extends Module
+{
+  public function __construct()
+  {
+    $this->name = 'dealt';
+    // $this->tab = 'administration';
+    $this->version = '1.0.0';
+    $this->author = 'Edvin CANDON';
+    $this->need_instance = 0;
+    $this->ps_versions_compliancy = [
+      'min' => '1.7',
+      'max' => '1.7.99',
+    ];
+    $this->bootstrap = true;
+
+    parent::__construct();
+    $this->displayName = $this->l('dealt');
+    $this->description = $this->l('The official Dealt prestashop module.');
+
+    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+    if (!Configuration::get('dealt')) {
+      $this->warning = $this->l('No name provided');
+    }
+  }
+
+  public function install()
+  {
+    return parent::install();
+  }
+
+  public function uninstall()
+  {
+    return parent::uninstall();
+  }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9bd33745098cb9926be0065f5b34500b470d9444',
+        'name' => 'dealt/dealt-prestashop',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dealt/dealt-prestashop' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9bd33745098cb9926be0065f5b34500b470d9444',
+            'dev_requirement' => false,
+        ),
+        'dealt/dealt-sdk' => array(
+            'pretty_version' => 'v0.0.2',
+            'version' => '0.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dealt/dealt-sdk',
+            'aliases' => array(),
+            'reference' => 'c44defe63c0a7ca07779df010d1715eabb21e3c8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
