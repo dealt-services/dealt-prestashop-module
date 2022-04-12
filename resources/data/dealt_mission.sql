c77baa2a5cb5b90096dad06db1ab569f51793c85
Edvin CANDON <edvincandon@gmail.com>
2022-04-12 11:32:19 +0200

    feat: ensure unique dealt mission id

--- resources/data/dealt_mission.sql
+++ resources/data/dealt_mission.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS `PREFIX_dealt_mission` (
   id_mission INT AUTO_INCREMENT NOT NULL,
   dealt_id_mission VARCHAR(255) NOT NULL,
   title_mission VARCHAR(255) NOT NULL,
-  description_mission VARCHAR(255),
-  date_added DATETIME NOT NULL,
-  date_updated DATETIME NOT NULL,
-  PRIMARY KEY(id_mission)
+  date_add DATETIME NOT NULL,
+  date_upd DATETIME NOT NULL,
+  PRIMARY KEY(id_mission),
+  UNIQUE (dealt_id_mission)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
