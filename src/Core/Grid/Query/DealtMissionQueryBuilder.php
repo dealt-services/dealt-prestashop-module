2a67f55d50072e75e72be49acb07001d8b9a38f9
Edvin CANDON <edvincandon@gmail.com>
2022-04-19 10:06:56 +0200

    feat: create and register DealtMissions query builder & grid data factory

---
+++ src/Core/Grid/Query/DealtMissionQueryBuilder.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Query;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+use PrestaShop\PrestaShop\Core\Grid\Query\AbstractDoctrineQueryBuilder;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+
+class DealtMissionQueryBuilder extends AbstractDoctrineQueryBuilder
+{
+  /**
+   * @param SearchCriteriaInterface|null $searchCriteria
+   * @return QueryBuilder
+   */
+  public function getSearchQueryBuilder(SearchCriteriaInterface $searchCriteria = null)
+  {
+    $qb = $this->getQueryBuilder($searchCriteria->getFilters());
+    $qb->select('dm.id_mission, dm.dealt_id_mission, dm.title_mission')
+      ->groupBy('dm.id_mission')
+      ->orderBy(
+        $searchCriteria->getOrderBy(),
+        $searchCriteria->getOrderWay()
+      );
+
+    if ($searchCriteria->getLimit() > 0) {
+      $qb
+        ->setFirstResult($searchCriteria->getOffset())
+        ->setMaxResults($searchCriteria->getLimit());
+    }
+
+    return $qb;
+  }
+
+  /**
+   * @param SearchCriteriaInterface|null $searchCriteria
+   * @return QueryBuilder
+   */
+  public function getCountQueryBuilder(SearchCriteriaInterface $searchCriteria = null)
+  {
+    $qb = $this->getQueryBuilder($searchCriteria->getFilters());
+    $qb->select('COUNT(dm.id_mission)');
+
+    return $qb;
+  }
+
+  /**
+   * @param array $filters
+   * @return QueryBuilder
+   */
+  private function getQueryBuilder(array $filters)
+  {
+    $allowedFilters = [
+      'id_mission',
+      'dealt_id_mission',
+      'title_mission',
+    ];
+
+    $qb = $this->connection
+      ->createQueryBuilder()
+      ->from($this->dbPrefix . 'dealt_mission', 'dm');
+
+    foreach ($filters as $name => $value) {
+      if (!in_array($name, $allowedFilters, true)) {
+        continue;
+      }
+
+      if ('id_mission' === $name || 'dealt_id_mission' === $name) {
+        $qb->andWhere('dm.`' . $name . '` = :' . $name);
+        $qb->setParameter($name, $value);
+
+        continue;
+      }
+
+      $qb->andWhere("$name LIKE :$name");
+      $qb->setParameter($name, '%' . $value . '%');
+    }
+
+    return $qb;
+  }
+}
