16d978d1b20f038809be5fb5fa994e40e46f3786
Edvin CANDON <edvincandon@gmail.com>
2022-04-22 12:15:54 +0200

    fix: ChoiceTree selector key

--- views/js/form/dealt.mission.form.js
+++ views/js/form/dealt.mission.form.js
@@ -1,4 +1,6 @@
 window.$(() => {
-  new window.prestashop.component.ChoiceTree("#form_mission_ids_category");
+  new window.prestashop.component.ChoiceTree(
+    "#dealt_mission_form_ids_category"
+  );
   new window.prestashop.component.TextWithLengthCounter();
 });
