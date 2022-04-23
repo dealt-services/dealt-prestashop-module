054f1a28e8bf012008de24d31da97ff2e30cc39e
Edvin CANDON <edvincandon@gmail.com>
2022-04-23 09:28:40 +0200

    fix: missing id_dealt_mission reference in dealt_virtual_product_category SQL

--- resources/data/dealt_virtual_product_category.sql
+++ resources/data/dealt_virtual_product_category.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS `PREFIX_dealt_virtual_product_category` (
+  id_dealt_mission INT NOT NULL,
   id_virtual_product INT NOT NULL,
   id_category INT NOT NULL
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
