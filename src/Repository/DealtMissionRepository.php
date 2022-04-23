cde1ad9cc6a58237091c01dbd3a24929b6091595
Edvin CANDON <edvincandon@gmail.com>
2022-04-23 08:29:45 +0200

    feat: add DealtMissionRepository & plug in Doctrine entity manager

---
+++ src/Repository/DealtMissionRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DealtModule\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+
+class DealtMissionRepository extends EntityRepository
+{
+}
