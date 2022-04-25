33280882d017b24a3a891ff9aa7bf1a0f1a1b5f8
Edvin CANDON <edvincandon@gmail.com>
2022-04-25 20:28:13 +0200

    feat: base CRUD methods in DealtMissionRepository & DealtVirtualProductRepository

--- src/Repository/DealtMissionRepository.php
+++ src/Repository/DealtMissionRepository.php
@@ -4,12 +4,87 @@ declare(strict_types=1);
 
 namespace DealtModule\Repository;
 
+use DealtModule\Entity\DealtMission;
+use DealtModule\Entity\DealtMissionCategory;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
+// use Doctrine\ORM\QueryBuilder;
 
 /**
  * Doctrine DealtMission repository class
  */
 class DealtMissionRepository extends EntityRepository
 {
+  /**
+   * Creates a Dealt Mission
+   * 
+   * @param string $missionTitle
+   * @param string $dealtMissionId
+   * @param int $virtualProductId
+   * @param array $categoryIds
+   * 
+   * @return DealtMission
+   */
+  public function create($missionTitle, $dealtMissionId, $virtualProductId, $categoryIds)
+  {
+    $em = $this->getEntityManager();
+
+    $mission = (new DealtMission())
+      ->setMissionTitle($missionTitle)
+      ->setDealtMissionId($dealtMissionId)
+      ->setVirtualProductId($virtualProductId)
+      ->setMissionCategoriesFromIds($categoryIds);
+
+    $em->persist($mission);
+    $em->flush();
+
+    return $mission;
+  }
+
+  /**
+   * Updates a Dealt Mission
+   * 
+   * @param int $missionId
+   * @param string $missionTitle
+   * @param string $dealtMissionId
+   * @param array $categoryIds
+   * 
+   * @return DealtMission
+   */
+  public function update($missionId, $missionTitle, $dealtMissionId, $categoryIds)
+  {
+    $em = $this->getEntityManager();
+
+    /** @var DealtMission */
+    $mission = ($this->findOneById($missionId))
+      ->setMissionTitle($missionTitle)
+      ->setDealtMissionId($dealtMissionId);
+
+    foreach ($mission->getMissionCategories() as $missionCategory) {
+      $this->deleteMissionCategory($missionCategory->getId());
+    }
+
+    $mission
+      ->clearMissionCategories()
+      ->setMissionCategoriesFromIds($categoryIds);
+
+    $em->persist($mission);
+    $em->flush();
+
+    return $mission;
+  }
+
+  /**
+   * Batch delete all categories associated with a dealt mission
+   * without flushing the Entity Manager ⚠️
+   *
+   * @param int $dealtMissionCategoryId
+   * @return void
+   */
+  private function deleteMissionCategory($dealtMissionCategoryId)
+  {
+    $em = $this->getEntityManager();
+    $dealtMissionCategory = $em->getPartialReference(DealtMissionCategory::class, $dealtMissionCategoryId);
+
+    $em->remove($dealtMissionCategory);
+  }
 }
--- src/Repository/DealtVirtualProductRepository.php
+++ src/Repository/DealtVirtualProductRepository.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace DealtModule\Repository;
 
 use Doctrine\DBAL\Connection;
+use DealtModule\Tools\Helpers;
+use PrestaShop\PrestaShop\Core\Domain\Product\Stock\ValueObject\OutOfStockType;
+use Product;
+use Category;
+use StockAvailable;
+
 
 /**
  * Repository class to help interacting with
@@ -36,7 +42,70 @@ class DealtVirtualProductRepository
     $this->dbPrefix = $dbPrefix;
   }
 
-  public function create()
+
+  /**
+   * @param integer $id
+   * @return Product
+   */
+  public function findOneById(int $id)
   {
+    $product = new Product($id);
+    return $product;
+  }
+
+  /**
+   * Create the Dealt mission virtual product
+   * 
+   * @param string $missionTitle
+   * @param string $dealtMissionId
+   * @param string $missionPrice
+   * 
+   * @return Product
+   */
+  public function create(string $missionTitle, string $dealtMissionId, string $missionPrice)
+  {
+    $category = Category::searchByName(null, '__dealt__', true);
+    $categoryId = $category['id_category'];
+
+    $product = new Product();
+    $product->reference = $dealtMissionId . '-dealt-product';
+    $product->name = Helpers::createMultiLangField($missionTitle);
+    $product->meta_description = '';
+    $product->visibility = 'none'; // we want to hide from the public catalog
+    $product->id_category_default = $categoryId;
+    $product->price = Helpers::formatPrice($missionPrice);
+    $product->minimal_quantity = 1;
+    $product->show_price = 1;
+    $product->on_sale = 0;
+    $product->online_only = 1;
+    $product->is_virtual = 1;
+
+    $product->add();
+
+    /* set stock available even when quantity = 0 */
+    $stockAvailable = new StockAvailable($product->id);
+    $stockAvailable->out_of_stock = OutOfStockType::OUT_OF_STOCK_AVAILABLE;
+    $stockAvailable->update();
+
+    return $product;
+  }
+
+  /**
+   * Updates the underlying product
+   *
+   * @param int $productId
+   * @param string $missionTitle
+   * @param string $missionPrice
+   * 
+   * @return Product
+   */
+  public function update(int $productId, string $missionTitle, string $missionPrice)
+  {
+    $product = new Product($productId);
+    $product->name  = Helpers::createMultiLangField($missionTitle);
+    $product->price = Helpers::formatPrice($missionPrice);
+    $product->save();
+
+    return $product;
   }
 }
