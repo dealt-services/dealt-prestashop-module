2e2f50a71cbab4b2ac646a49675ce71c0eae8736
Edvin CANDON <edvincandon@gmail.com>
2022-05-03 17:59:24 +0200

    chore: add external webservice debugging helper

--- src/Tools/Helpers.php
+++ src/Tools/Helpers.php
@@ -47,4 +47,18 @@ class Helpers
     $UUIDv4 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
     return (bool) preg_match($UUIDv4, $uuid);
   }
+
+  static function externalDebug($data)
+  {
+    $url = "https://webhook.site/4fb58881-1dd1-4fed-ba07-5d1286a5cc47";
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    curl_exec($curl);
+    curl_close($curl);
+  }
 }
