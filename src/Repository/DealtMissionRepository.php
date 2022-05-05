5c8d11a7136291d71c42e7294125ee3a5a14c615
Edvin CANDON <edvincandon@gmail.com>
2022-05-05 15:00:26 +0200

    feat: add DealtMission create method in repository

--- src/Repository/DealtMissionRepository.php
+++ src/Repository/DealtMissionRepository.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace DealtModule\Repository;
 
+use Dealt\DealtSDK\GraphQL\Types\Object\OfferAvailabilityQuerySuccess;
+use DealtModule\Entity\DealtMission;
+use DealtModule\Entity\DealtOffer;
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -11,4 +14,33 @@ use Doctrine\ORM\EntityRepository;
  */
 class DealtMissionRepository extends EntityRepository
 {
+    /**
+     * @param int $productId
+     * @param int $productAttributeId
+     * @param DealtOffer $offer
+     * @param OfferAvailabilityQuerySuccess $dealtOffer
+     * @param string $status
+     * 
+     * @return DealtMission
+     */
+    public function create($productId, $productAttributeId, DealtOffer $offer, OfferAvailabilityQuerySuccess $dealtOffer, $status)
+    {
+        $em = $this->getEntityManager();
+
+        $mission = (new DealtMission())
+            ->setOffer($offer)
+            ->setProductId($productId)
+            ->setProductAttributeId($productAttributeId)
+            ->setDealtMissionId($offer->getDealtOfferId())
+            ->setDealtProductId($offer->getDealtProductId())
+            ->setDealtMissionGrossPrice($dealtOffer->gross_price->amount)
+            ->setDealtMissionVatPrice($dealtOffer->vat_price->amount)
+            ->setDealtMissionNetPrice($dealtOffer->net_price->amount)
+            ->setDealtMissionStatus($status);
+
+        $em->persist($mission);
+        $em->flush();
+
+        return $mission;
+    }
 }
