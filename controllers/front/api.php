5c60848fe48a4552fa4ecbfe15441340c331bce9
Edvin CANDON <edvincandon@gmail.com>
2022-05-17 16:46:03 +0200

    fix: lowercase front controller files

---
+++ controllers/front/api.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+use DealtModule\Action\DealtAPIAction;
+use DealtModule\Controller\Front\ModuleActionHandlerFrontController;
+use DealtModule\Repository\DealtMissionRepository;
+
+class DealtModuleApiModuleFrontController extends ModuleActionHandlerFrontController
+{
+    public function getModuleActionsClass()
+    {
+        return get_class(new DealtAPIAction());
+    }
+
+    public function handleAction($action)
+    {
+        switch ($action) {
+            case DealtAPIAction::$OFFER_AVAILABILITY:
+                return $this->handleOfferAvailability();
+
+            case DealtAPIAction::$MISSION_WEBHOOK:
+                return $this->handleMissionWebhook();
+        }
+
+        throw new Exception('something went wrong while handling API action');
+    }
+
+    protected function handleOfferAvailability()
+    {
+        $client = $this->module->getAPIService();
+        $dealtOffer = $client->checkAvailability(strval(Tools::getValue('dealt_id_offer')), strval(Tools::getValue('zip_code')));
+
+        if ($dealtOffer == null) {
+            throw new Exception('Unable to check offer availability');
+        }
+
+        return array_merge(
+            ['available' => $dealtOffer->available],
+            $dealtOffer->available ? [] : ['reason' => $this->trans('Offer unavailable for the requested zip code', [], 'Modules.Dealtmodule.Shop')]
+        );
+    }
+
+    /**
+     * Make sure the webhook is triggered with the
+     * appropriate header in order to read the JSON body :
+     *
+     * Content-Type:application/json
+     *
+     * @return mixed
+     */
+    protected function handleMissionWebhook()
+    {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+
+        $dealtMissionId = $data['missionId'] ?? false;
+        $dealtMissionStatus = $data['status'] ?? false;
+
+        if ($dealtMissionId == false || $dealtMissionStatus == false) {
+            throw new Exception('Dealt webhook failed parsing POST body');
+        }
+
+        /** @var DealtMissionRepository */
+        $missionRepo = $this->module->get('dealtmodule.doctrine.dealt.mission.repository');
+        $missionRepo->updateStatusByDealtMissionId($dealtMissionId, $dealtMissionStatus);
+
+        return [
+            'dealtMissionId' => $dealtMissionId,
+            'dealtMissionStatus' => $dealtMissionStatus,
+        ];
+    }
+}
---
+++ controllers/front/cart.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+use DealtModule\Action\DealtCartAction;
+use DealtModule\Controller\Front\ModuleActionHandlerFrontController;
+
+class DealtModuleCartModuleFrontController extends ModuleActionHandlerFrontController
+{
+    public function getModuleActionsClass()
+    {
+        return get_class(new DealtCartAction());
+    }
+
+    public function handleAction($action)
+    {
+        switch ($action) {
+      case DealtCartAction::$ADD_TO_CART:
+        return $this->handleAddToCart();
+      case DealtCartAction::$DETACH_OFFER:
+        return $this->handleDetachOffer();
+      case DealtCartAction::$GET_PRODUCT_ATTRIBUTE_ID:
+        return $this->handleGetProductAttributeId();
+    }
+
+        throw new Exception('something went wrong while handling Cart action');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function handleAddToCart()
+    {
+        return $this->module->getCartService()->addDealtOfferToCart(
+      strval(Tools::getValue('dealt_id_offer')),
+      intval(Tools::getValue('id_product')),
+      intval(Tools::getValue('id_product_attribute'))
+    );
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    protected function handleDetachOffer()
+    {
+        return $this->module->getCartService()->detachDealtOffer(
+      strval(Tools::getValue('dealt_id_offer')),
+      intval(Tools::getValue('id_product')),
+      intval(Tools::getValue('id_product_attribute'))
+    );
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    protected function handleGetProductAttributeId()
+    {
+        return [
+      'productAttributeId' => (int) Product::getIdProductAttributeByIdAttributes(
+        Tools::getValue('id_product'),
+        Tools::getValue('group')
+      ),
+    ];
+    }
+}
